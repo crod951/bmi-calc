bb5d2652f30f884d97f1e5db361b1839f951553b
Chris Rodrigues <crod951@gmail.com>
2025-08-31 18:52:45 -0700

    fix: remove placeholder on focus in input; adjusted opacity for placeholder to differentiate it more from input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -19,10 +19,10 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       data-slot="input"
       onKeyDown={handleKeyDown}
       className={cn(
-        "file:text-gray-900 dark:file:text-gray-100 placeholder:text-gray-500/40 dark:placeholder:text-gray-400/40 selection:bg-teal-600 selection:text-white flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base shadow-sm transition-colors outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "file:text-gray-900 selection:bg-teal-600 selection:text-white flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base shadow-sm transition-colors outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         isDark 
-          ? "border-gray-600 bg-gray-800 text-gray-100 placeholder:text-gray-400" 
-          : "border-slate-200 bg-gray-100 text-gray-900 placeholder:text-gray-500",
+          ? "border-gray-600 bg-gray-800 text-gray-100 placeholder:text-gray-400/20 focus:placeholder:text-transparent" 
+          : "border-slate-200 bg-gray-100 text-gray-900 placeholder:text-gray-500/20 focus:placeholder:text-transparent",
         "focus:border-teal-500 focus:ring-2 focus:ring-teal-500/50",
         "aria-invalid:ring-red-500/20 aria-invalid:border-red-500",
         className
